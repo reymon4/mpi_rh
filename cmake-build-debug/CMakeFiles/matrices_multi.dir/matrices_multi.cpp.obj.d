9028fad51feaaf3418ffa1cb40f6a4f03e235ed5
Cristina-Colcha <81483322+NelsonSoria@users.noreply.github.com>
2025-01-08 07:23:16 -0500

    Clase del 8/1/25

--- cmake-build-debug/CMakeFiles/matrices_multi.dir/matrices_multi.cpp.obj.d
+++ cmake-build-debug/CMakeFiles/matrices_multi.dir/matrices_multi.cpp.obj.d
@@ -1,20 +1,19 @@
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/iostream
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/yvals_core.h
+C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpi.h
+C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpio.h
+C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpi.h
+C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpicxx.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vcruntime.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/sal.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/concurrencysal.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vadefs.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_stdio_config.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/memory
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/yvals_core.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xkeycheck.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/istream
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/ostream
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/ios
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xlocnum
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cfloat
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/float.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/climits
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/limits.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cmath
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/exception
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/yvals.h
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/crtdbg.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vcruntime_new_debug.h
@@ -29,15 +28,18 @@ C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_malloc.
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_search.h
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stddef.h
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdlib.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/limits.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/type_traits
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstddef
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xtr1common
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/intrin0.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/intrin0.inl.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstdio
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdio.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdio.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_stdio_config.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/iterator
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstdint
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/stdint.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/malloc.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vcruntime_exception.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/eh.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_terminate.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/iosfwd
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstdio
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstring
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/string.h
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_memory.h
@@ -56,14 +58,28 @@ C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wproces
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wtime.h
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/sys/stat.h
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/sys/types.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/typeinfo
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vcruntime_typeinfo.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xmemory
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/limits
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cfloat
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/float.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/climits
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/intrin0.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/intrin0.inl.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/new
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xatomic.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xutility
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/__msvc_iter_core.hpp
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/utility
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/initializer_list
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstddef
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/type_traits
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cstdint
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/stdint.h
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/iostream
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/istream
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/ostream
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/ios
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xlocnum
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cmath
+C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/iterator
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/streambuf
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xiosbase
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/share.h
@@ -71,18 +87,9 @@ C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.3412
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/__msvc_system_error_abi.hpp
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/cerrno
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/stdexcept
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/exception
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/malloc.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vcruntime_exception.h
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/eh.h
-C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_terminate.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xstring
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/__msvc_sanitizer_annotate_container.hpp
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/__msvc_string_view.hpp
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xmemory
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/limits
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/new
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xatomic.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xpolymorphic_allocator.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/tuple
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xcall_once.h
@@ -94,9 +101,6 @@ C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.3412
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/ctime
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/time.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xlocale
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/memory
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/typeinfo
-C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/vcruntime_typeinfo.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xfacet
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/xlocinfo
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/__msvc_xlocinfo_types.hpp
@@ -104,10 +108,6 @@ C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.3412
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/ctype.h
 C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/include/clocale
 C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/locale.h
-C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpi.h
-C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpio.h
-C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpi.h
-C:/Program Files (x86)/Intel/oneAPI/mpi/latest/include/mpicxx.h
 C:/Users/fing.labcom/.conan2/p/b/fmt8ca07962271e4/p/include/fmt/core.h
 C:/Users/fing.labcom/.conan2/p/b/fmt8ca07962271e4/p/include/fmt/format.h
 C:/Users/fing.labcom/.conan2/p/b/fmt8ca07962271e4/p/include/fmt/base.h
--- matrices_multi.cpp
+++ matrices_multi.cpp
@@ -1,83 +1,268 @@
+#include <mpi.h>
+#include <memory>
 #include <iostream>
+#include <cstdlib> // Para rand() y srand()
+#include <ctime>   // Para time()
+#include <fmt/core.h>
 
-#include<mpi.h>
-#include<fmt/core.h>
-#include<memory>
 #define MATRIX_DIM 25
 
-//nprocs=4
-//rows_per_rank=25/4=6
-//
-void printVector(double* vec, int size)
-{
-    fmt::print("[");
-    for(int i=0; i<size; i++)
-    {
-        fmt::print("{}, ", vec[i]);
+void imprimir_vector(const std::string msg, double* v, int size) {
+    fmt::print("{} [",msg);
+        for (int i=0; i<size;i++) { //imprime los numeros seguidos de una coma
+            fmt::print("{},",v[i]);
+        }
+
+    fmt::println("]");
+}
+
+void imprimir_matriz(const std::string msg,double* A,int rows, int cols) {
+    fmt::print("{} [",msg);
+    for (int i=0;i<rows;i++) {
+        fmt::print("{:>2}:  ",i);
+        for (int j=0; j<cols;j++) {
+            int index = i*cols+j;
+            fmt::print("{:2},",A[index]);
+        }
+        fmt::println("");
     }
     fmt::println("]");
 }
-int main(int argc, char** argv)
-{
-    MPI_Init(&argc, &argv);
 
-    int rank;
-    int nprocs;
+void multiplicar_matriz_vector(double* A, double* x, double* b, int rows, int cols) {
+    for (int i=0; i<rows; i++) {
+        b[i]=0;
+        for (int j=0; j<cols; j++) {
+            b[i]+=A[i*cols+j]*x[j];
+        }
+    }
+}
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); //id del proceso
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);//# de proceso
+int main(int argc, char* argv[]) {
 
-    int rows_per_rank; //rows per rank
-    int rows_alloc= MATRIX_DIM; //TAMAÑO AJUSTADO DE LA MATRIZ PARA QUE EL NUM DE FILAS SEA DIVISIBLE PARA EL NPROS
-    int padding=0; //padding for the last rank (Relleno)
 
-    if(MATRIX_DIM % nprocs != 0) //El número de filas no es divisible para nproces. Entonces, necesitamos relleno en el último rank
-    {
-        rows_alloc = std::ceil((double)MATRIX_DIM / nprocs) * nprocs;
-        padding = rows_alloc - MATRIX_DIM;
+    MPI_Init(&argc, &argv); // Inicializa MPI
+    int rank, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtener el rank del proceso
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); // Obtener el número total de procesos
+
+
+    int rows_per_rank; //Numero de filas a enviar por cada RANK
+    int rows_alloc=MATRIX_DIM; //tamanio ajustado de la matriz, para que el # de filas sea divisipa para el NPROCS
+    int padding=0; //numero de filas que se agregan para ajustar el tamanio de la matriz
+
+    if(MATRIX_DIM%nprocs!=0) {
+        //el numero de filas no es divisiple para NPROCS
+        rows_alloc=std::ceil((double) MATRIX_DIM/nprocs)*nprocs; //funcion techo de la division y multiplicacion por el numero de procesos
+        padding=rows_alloc-MATRIX_DIM; //numero de filas que se agregan para ajustar el tamanio de la matriz
     }
 
-    rows_per_rank = rows_alloc / nprocs;
-    //Buffers -> smart pointers
-    //b=A*x
-    std::unique_ptr<double[]> A;
-    std::unique_ptr<double[]> B;
-    std::unique_ptr<double[]> x = std::make_unique<double[]>(MATRIX_DIM); //Reservamos memoria
+    rows_per_rank=rows_alloc/nprocs; //numero de filas a enviar por cada RANK
 
-    //bi = Ai * x, donde Ai es la matriz de dimension 4*25 -> rows_per_rank*25
+    //--buffers
+    // b= A*x
+    std::unique_ptr<double[]> A; //solo RANK_0
+    //esto es un puntero a un array de doubles
+    std::unique_ptr<double[]> b; //solo RANK_0
+    std::unique_ptr<double[]> x=std::make_unique<double[]>(MATRIX_DIM);
+        //permite reservar memoria
+
+    //bi = Ai*x, donde Ai e suna matriz de dimencion 7x25 ==> rows_per_rank x MATRIX_DIM
+    /*
+    A_0 = [
+        f0
+        f1
+        f2
+        f3
+        f4
+        f5
+        f6
+    ]
+
+    A_1 = [
+        f7
+        f8
+        f9
+        f10
+        f11
+        f12
+        f13
+    ]
+
+    A_2 = [
+        f14
+        f15
+        f16
+        f17
+        f18
+        f19
+        f20
+    ]
+
+    A_3 = [
+        f21
+        f22
+        f23
+        f24
+        f25 --> padding
+        f26 --> padding
+        f27 --> padding
+    ]
+
+     */
+
     std::unique_ptr<double[]> A_local;
     std::unique_ptr<double[]> b_local;
 
-    //Cada rank va a tener siete filas de la matriz A
+    if(rank==0) {
+        fmt::print("Dimension={}, rows_alloc={}, rows_per_rank={}, padding={}\n",
+            MATRIX_DIM, rows_alloc, rows_per_rank, padding
+            );
 
-    if(rank==0)
-    {
-        fmt::println("Dimension={}, rows_alloc={}, rows_per_rank={}, padding={}", MATRIX_DIM, rows_alloc, rows_per_rank, padding);
-        //std::printf("Dimension=%d, rows_alloc=%d, rows_per_rank=%d, padding=%d\n", MATRIX_DIM, rows_alloc, rows_per_rank, padding);
-        //Relleno por matriz
-        A = std::make_unique<double[]>(rows_alloc*MATRIX_DIM);
-        B = std::make_unique<double[]>(rows_alloc);
-        for(int i=0; i<MATRIX_DIM; i++){
-            fmt::print("fila={:>2} -> ",i);
-            for(int j=0; j<MATRIX_DIM; j++){
-               int index = i*MATRIX_DIM + j;
-                A[index] = i;
-                fmt::print("{:2} ",A[index]);
+        A=std::make_unique<double[]>(rows_alloc*MATRIX_DIM); //defino la matriz A con dimencion 28*25
+        //al manejar la matriz en una sola linea vectorial me facilita la distribucion en los ranks.
+        b=std::make_unique<double[]>(rows_alloc); //defino b con 28 elemenetos
+
+        //--incializar matriz A,, vector x
+         //std::printf("         c1 c2 c3 ...\n");
+         for(int i=0; i<MATRIX_DIM; i++) {
+             //fmt::print("fila{:>3} |",i);
+             for(int j=0; j<MATRIX_DIM; j++) {
+                 int index = i*MATRIX_DIM + j;
+                 A[index]=i;
+                 //fmt::print( "{:>2} ", A[index]);
+             }
+             //std::printf("| \n");
+         }// define la matriz A con valores de 0 a 24 en cada fila
+
+        //imprimir_matriz(A,MATRIX_DIM,MATRIX_DIM);
+
+        for (int i=0; i<MATRIX_DIM; i++) {
+            x[i]=1;
+        }// defino el vector x con valores de 1
+
+
+
+        // Rellenar filas adicionales si hay padding /////#############################################
+        /*for (int i = MATRIX_DIM; i < rows_alloc; i++) {
+            for (int j = 0; j < MATRIX_DIM; j++) {
+                A[i * MATRIX_DIM + j] = 0; // Llenar con ceros
             }
-            fmt::println("");
-        }
-        for(int i=0; i<MATRIX_DIM; i++){
-            x[i] = i;
-        }
+        }*/
+        /////////////////////////////////////////////////////////////////////////////
     }
-    A_local = std::make_unique<double[]>(rows_per_rank*MATRIX_DIM);
-    b_local = std::make_unique<double[]>(rows_per_rank);
 
-    printVector(x.get(), MATRIX_DIM);
+    //--incializar matrizes locales
+    A_local=std::make_unique<double[]>(rows_per_rank*MATRIX_DIM); //defino la matriz A con dimencion 7*25
+    // es decir en este caso ya tomo unicamente las filas de la matriz A que le corresponde a cada rank
+    // En resumen: es la submatriz que maneja un rank x
+    b_local=std::make_unique<double[]>(rows_per_rank); // como tengo 7 filas a multiplicar, por tanto tengo 7 resultados (rows_per_rank=7)
+    // En resumen es el resultado parcial de la multiplicacion.
 
+    //imprimir vector 'x'
+    auto txt = fmt::format("RANK_{}",rank);
+    imprimir_vector(txt,x.get(),MATRIX_DIM);
 
     MPI_Bcast(x.get(), MATRIX_DIM, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    //Distribuye el vector x desde el proceso rank 0 a todos los demás procesos. root=0 es decir el rank 0
+
+    auto txt_after = fmt::format("RANK_{} after",rank);
+    imprimir_vector(txt_after,x.get(),MATRIX_DIM);
+
+
+
+
+
+    ////////////////////////////////////////////////////////
+
+    /*// Dividir la matriz A entre los procesos
+    if (rank == 0) {
+        MPI_Scatter(A.get(), rows_per_rank * MATRIX_DIM, MPI_DOUBLE,
+                    MPI_IN_PLACE, rows_per_rank * MATRIX_DIM, MPI_DOUBLE,
+                    0, MPI_COMM_WORLD);
+    } else {
+        MPI_Scatter(nullptr, rows_per_rank * MATRIX_DIM, MPI_DOUBLE,
+                    A_local.get(), rows_per_rank * MATRIX_DIM, MPI_DOUBLE,
+                    0, MPI_COMM_WORLD);
+    }
+
+    // Multiplicación local: b_local = A_local * x
+    for (int i = 0; i < rows_per_rank; i++) {
+        b_local[i] = 0.0; // Inicializar a 0
+        for (int j = 0; j < MATRIX_DIM; j++) {
+            b_local[i] += A_local[i * MATRIX_DIM + j] * x[j];
+        }
+    }
+
+    // Recolectar los resultados locales en el proceso raíz
+    if (rank == 0) {
+        MPI_Gather(MPI_IN_PLACE, rows_per_rank, MPI_DOUBLE,
+                   b.get(), rows_per_rank, MPI_DOUBLE,
+                   0, MPI_COMM_WORLD);
+    } else {
+        MPI_Gather(b_local.get(), rows_per_rank, MPI_DOUBLE,
+                   nullptr, rows_per_rank, MPI_DOUBLE,
+                   0, MPI_COMM_WORLD);
+    }
+
+    // Imprimir el vector resultado b en el proceso raíz
+    if (rank == 0) {
+        fmt::print("Vector resultado b:\n");
+        imprimir_vector("", b.get(), MATRIX_DIM);
+    }*/
+
+    ///////////////////////
+
+
+    //--enviar la matriz A: Scatter
+    MPI_Scatter(
+        A.get(),
+        MATRIX_DIM*rows_per_rank,
+        MPI_DOUBLE,
+        A_local.get(),
+        MATRIX_DIM*rows_per_rank,
+        MPI_DOUBLE,
+        0,
+        MPI_COMM_WORLD);
+
+    int rank_to_print=0;
+    if (rank==rank_to_print) {
+        auto txtMatriz = fmt::format("RANK_{} Mtz",rank);
+        imprimir_matriz(txtMatriz,A_local.get(),rows_per_rank,MATRIX_DIM);
+    }
+
+    // -- calcular D_local = A_local * x
+    int rows_per_rank_tmp=rows_per_rank;
+    if(rank==nprocs-1) {
+        rows_per_rank_tmp=rows_per_rank-padding;
+    }
+
+    multiplicar_matriz_vector(A_local.get(),x.get(),b_local.get(),rows_per_rank_tmp,MATRIX_DIM);
+
+    if (rank==rank_to_print) {
+        auto txtVector = fmt::format("RANK_{} b_local",rank);
+        imprimir_vector(txtVector,b_local.get(),rows_per_rank);
+    }
+
+    //--enviar los resultados parciales al RANK_0: Gather
+    MPI_Gather(
+        b_local.get(), //enviar
+        rows_per_rank,
+        MPI_DOUBLE,
+        b.get(), //recibir
+        rows_per_rank,
+        MPI_DOUBLE,
+        0, //al RANK_0
+        MPI_COMM_WORLD
+    );
+
+    if (rank==0) {
+        auto txtResultado = fmt::format("RANK_{} rasultado b: ",rank);
+        imprimir_vector(txtResultado,b.get(),MATRIX_DIM);
+    }
+
 
     MPI_Finalize();
     return 0;
+
 }
